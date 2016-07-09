a925051af66be57b0f19ff92c9a3fd0a3765fbd0
tanzirhasan <hitanzir@gmail.com>
2016-07-09 03:41:06 -0700

    fda url

--- src/www/views/result/result.js
+++ src/www/views/result/result.js
@@ -1,4 +1,8 @@
 angular.module('App')
     .controller('ResultCtrl', function ($scope, $http) {
-
+        $scopeinfo=[];
+        $http.get('https://api.fda.gov/drug/label.json')
+            .success(function (response) {
+                $scope.info.push(element)
+            })
     });
