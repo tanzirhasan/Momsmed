c0104863287ef8088ec3fd90faca2a62c2d54076
tanzirhasan <hitanzir@gmail.com>
2016-07-12 17:12:51 -0700

    New changes

--- src/www/views/result/result.js
+++ src/www/views/result/result.js
@@ -1,13 +1,14 @@
 angular.module('App')
     .controller('ResultCtrl', function ($scope, $http, $ionicLoading) {
-        $ionicLoading.show();
+            $ionicLoading.show();
 
-        // var url = 'https://api.fda.gov/drug/label.json:'
-        var   med ='?search=amlodipin'
-        $http.get('/api/drugs' + med)
-            .success(function (medinfo) {
-                $scope.medinfo = medinfo;
-                console.log($scope.medinfo.results[0].pregnancy[0].indexOf('Category'))
-            })
-        $ionicLoading.hide();
+            // var url = 'https://api.fda.gov/drug/label.json:'
+            var med = '?search=amlodipin'
+            $http.get('/api/drugs' + med)
+                .success(function (medinfo) {
+                    $scope.medinfo = medinfo;
+                    console.log($scope.medinfo.results[0].pregnancy[0].indexOf('Category'))
+                })
+            $ionicLoading.hide();
+
     });
